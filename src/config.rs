7ee73a0313c7b7fc4d6b71a6de396e4e523029bb
bbfchen <bbf.ccc@gmail.com>
2025-06-22 20:47:16 -0400

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["kiska.bbfnet.net"];
+pub const RS_PUB_KEY: &str = "fynsg+PeWnME0Wky7UAK9o2eoHjNK1GkkhUrySUFCng=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
