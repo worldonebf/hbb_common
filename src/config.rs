6c63872844474fa5a154c6eaf8a378f946a00e06
jelly0925 <jelly0925@gmail.com>
2025-08-15 14:50:10 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.666521.xyz"];
+pub const RS_PUB_KEY: &str = "auXO3syyBPtAFgqGsZzVwc7E3pof5SCc3OEALuoXIRo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
