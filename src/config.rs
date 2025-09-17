c907c5ff402e59bba3ccd6c87dd2cdd6af4a78db
suoer2025 <24334351@qq.com>
2025-09-17 15:37:15 -0600

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["81.137.81.205"];
+pub const RS_PUB_KEY: &str = "vuNsvLZlWZ6w8kPx8HhCVFJNXRxTRc6daxTMOMfmdNE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
